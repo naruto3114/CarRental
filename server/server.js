eada78a8efb4ea40f2128eacc5062e0a876af67a
naruto3114 <140521613+naruto3114@users.noreply.github.com>
2026-03-20 13:22:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,26 +1,37 @@
 import express from 'express';
 import "dotenv/config";
 import cors from "cors";
+import ImageKit from "imagekit"; // 1. Import ImageKit
 import connectDB from './configs/db.js';
 import userRouters from './routes/userRoutes.js';
 import ownerRouter from './routes/ownerRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 
-//Initialize Express App
-
 const app = express()
 
-// connect database
 await connectDB()
 
-//Middleware
+// 2. Initialize ImageKit
+const imagekit = new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+});
+
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req ,res)=> res.send("Server is running"))
+
+// 3. ImageKit Auth Endpoint (For Frontend Uploads)
+app.get('/api/imagekit-auth', (req, res) => {
+    const result = imagekit.getAuthenticationParameters();
+    res.send(result);
+});
+
 app.use('/api/user', userRouters)
 app.use('/api/owner', ownerRouter)
 app.use('/api/bookings', bookingRouter)
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> console.log(`Server runnign on port ${PORT}`))
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
